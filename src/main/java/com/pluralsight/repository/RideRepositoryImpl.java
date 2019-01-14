218286c72380a45e1f3079d4a998f8d5218d1ae2
Shubham Gupta <bittu.rocks2@yahoo.com>
2019-01-14 18:25:57 +0530

    till module 4

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -3,13 +3,18 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
 
-@Repository("rideRepository")
+@Repository("rideRepository") //repository layer communicate with the database
 public class RideRepositoryImpl implements RideRepository {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate; //injecting bean from xml
+	
 	@Override
 	public List<Ride> getRides() {
 		Ride ride = new Ride();
